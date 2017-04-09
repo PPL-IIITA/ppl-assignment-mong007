6b791ff1e930399fe04a1c09bb3b957bf050c429
VISHAL KUMAR SINGH <mong007@users.noreply.github.com>
2017-04-09 11:28:23 +0530

    Final Commit

---
+++ Q4/girl/girl.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * This is girl package.
+ */
+package girl;
+
+/**
+ *This class contains and manages attributes of Girls.
+ * @author vishal kumar singh iit2015141
+ */
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public abstract class girl {
+	public String name; 
+	public int budget;	
+	public int intel;
+	public int attract;
+        public int bf_id;
+        public int rec;
+	public String bf = " ";
+	public double happ;
+	public char status = 's';
+	public girl(String name1, int intel1, int attract1, int budget1){
+            super();
+            this.name = name1;
+            this.intel = intel1;
+            this.attract = attract1;
+            this.budget = budget1;
+            this.bf_id = -1;
+            this.rec = 0;
+            this.bf = " ";
+            this.happ = 0;
+            this.status = 's';
+            }
+        /**  
+    * It check whether a boy is eligible to be BF or not.  
+    * @param b_budget It is budget of the enquired Boy.  
+     * @return   Returns 1 for Yes and 0 for no.
+    */ 
+	public boolean is_eligible (int b_budget) {
+		if(b_budget >= this.budget) 
+			return true;
+		else 
+			return false;
+	}
+	/**  
+    * It returns relationship status of girl.   
+     * @return   Returns relationship status of girl.
+    */ 
+	public char view_status() {
+		return this.status;
+	}
+        /**  
+         * It changes relationship status of girl . 
+         @param status1 It is new relationship status of girl . 
+    */
+	public void change_status(char status1) {
+			  if(status1 == 'c') this.status = 'c';
+			   else this.status = 's';
+	} 
+        public abstract double happi(int x);
+}
---
+++ Q4/testgen/testgen.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgen;
+
+/**
+ *This Class generates random inputs.
+ * @author vishal kumar singh iit2015141
+ */
+import java.io.*;
+import java.util.*;
+public class testgen {
+    /**
+     * This function generates random inputs.
+     */
+	    public void generate(){
+		try{
+                        String[] type_b = {"miser", "generous", "geeks"};
+                        String[] type_g = {"choosy", "normal", "desperate"};
+                        String[] type_gi = {"essential", "luxury", "utility"};
+			FileWriter file1 = new FileWriter("boy.txt");
+			FileWriter file2 = new FileWriter("girl.txt");
+                        FileWriter file3 = new FileWriter("gift.txt");
+			Random b = new Random();
+			Random g = new Random();
+                        Random gi = new Random();
+			int j;
+		for(j=0;j<100;j++){
+				file1.write("B"+j+" "+(b.nextInt(100)+5)+" "+(b.nextInt(100)+5)+" "+(b.nextInt(100)+5)+" "+(b.nextInt(10000)+5)+" "+type_b[b.nextInt(type_b.length)]+"\n");
+			}
+			for(j=0;j<100;j++){
+				file2.write("G"+j+" "+(g.nextInt(100)+5)+" "+(g.nextInt(1000)+5)+" "+(g.nextInt(100)+5)+" "+type_g[g.nextInt(type_g.length)]+"\n");
+			}
+                        for(j=0;j<100;j++){
+				file3.write(type_gi[gi.nextInt(type_gi.length)]+" "+(gi.nextInt(100)+5)+"\n");
+			}
+			file1.close();
+			file2.close();
+                        file3.close();
+		}catch(IOException e){		
+			System.out.println( e.getMessage() );
+		}
+	
+		
+	}
+	    
+}
